17d7f09d9483a8fda4f3c012a745d419818fa235
Jeremy Her <91761028+herj043@users.noreply.github.com>
2022-02-11 23:28:04 +0000

    Messed around with code

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-import tkinter as tk
-import schedule 
+import schedule as date
+date.dope()
+def guess():
+  win = input("Type Something: ")
+  
+  if "OP".lower() in win.lower():
+    print("yes")
+  else:
+    guess()
 
+guess()
+cont = "zero"
+while "no".lower() not in cont.lower():
+  cont = input("Would you like to play again? (Yes/No) ")
+  if "yes" in cont.lower():
+    guess()
+  if "no" in cont.lower():
+    print("Thanks for testing!")
+  else:
+    print("unaccepted statement")
--- schedule.py
+++ schedule.py
@@ -1,1 +1,3 @@
-import calendar
+def dope():
+  print("dope")
+  pass
